b602cbfc9554c86a3f2d51c90989085d7448af13
BarPosWasTaken <ketsopb@gmail.com>
2020-11-07 12:50:00 +0100

    invite

---
+++ commands/misc/invite.js
@@ -0,0 +1,22 @@
+// https://discord.com/api/oauth2/authorize?client_id=769630010020593674&permissions=8&scope=bot
+const {MessageEmbed} = require('discord.js')
+const config = require('../../config.json')
+
+module.exports = {
+    commands: ['invite'],
+    //expectedArgs: '<num1> <num2>',
+    //permissionError: 'You need admin permissions to run this command',
+    minArgs: 0,
+    maxArgs: 0,
+    callback: (message, arguments, text, client) => {
+        const embed = new MessageEmbed()
+            .setTitle(`Click here to invite me!`)
+            .setURL(`https://discord.com/api/oauth2/authorize?client_id=769630010020593674&permissions=8&scope=bot`)
+            .setTimestamp()
+
+        message.channel.send(embed)
+    },
+    //permissions: 'ADMINISTRATOR',
+    //requiredRoles: [],
+    //allowedUsers: '437992463165161472'
+  }
