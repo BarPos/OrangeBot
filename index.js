8fbe0f2e4c3133af1cb6f2cc0328de18ebd12b6f
BarPosWasTaken <ketsopb@gmail.com>
2020-12-06 16:57:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 
 // ? VERSION
-const version = 'stable-v1.4.6'
+const version = 'stable-v1.4.7'
 // ? ###
 
 const l = require('./logs');
