2fba2d92633248c61a254a62c799c2ef06430045
BarPosWasTaken <ketsopb@gmail.com>
2020-12-06 17:33:29 +0100

    Update update.js

--- commands/developer/update.js
+++ commands/developer/update.js
@@ -26,7 +26,11 @@ module.exports = {
             .setColor(config.color)
             .setDescription(stdout)
             .setTimestamp()
-        await m.edit(embed);
+        await m.edit(embed)
+        if(code !== 0){
+            return;
+        }
+        // }
         await message.channel.send('Restarting...');
         shell.exec('pm2 restart orange');
     },
