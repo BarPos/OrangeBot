ab05d0a878c8602cf86bbcde6645ce5858223697
BarPosWasTaken <ketsopb@gmail.com>
2020-12-06 16:40:18 +0100

    update

---
+++ commands/developer/update.js
@@ -0,0 +1,29 @@
+const util = require('util');
+const {client} = require('../../index');
+const config = require('../../config.json')
+const shell = require('shelljs')
+
+module.exports = {
+    commands: 'update',
+    expectedArgs: '<eval>',
+    permissionError: 'You need admin permissions to run this command',
+    minArgs: 0,
+    maxArgs: 0,
+    callback: async (message, arguments, text, client) => {
+        if (!shell.which('git')) {
+            shell.echo('Sorry, this script requires git');
+            shell.exit(1);
+            return
+        }
+        if (shell.exec('git pull https://github.com/BarPos/OrangeBot.git"').code !== 0) {
+            shell.echo('Error: Git pull failed');
+            shell.exit(1);
+            return
+        }
+        message.channel.send('Restarting...');
+        shell.exec('pm2 restart orange');
+    },
+    //permissions: 'ADMINISTRATOR',
+    //requiredRoles: [],
+    allowedUsers: '437992463165161472'
+  }
